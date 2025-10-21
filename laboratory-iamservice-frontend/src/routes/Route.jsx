c21175bd568fa61c63249b24b64c1781c9d012c1
wishuponsirius <nnthienkim2905@gmail.com>
2025-10-22 00:39:18 +0700

    [FEAT] Base setup for protected route]

--- laboratory-iamservice-frontend/src/routes/Route.jsx
+++ laboratory-iamservice-frontend/src/routes/Route.jsx
@@ -1,20 +1,36 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import TestPages from "../pages/TestPages";
 import RoleList from "../pages/role/RoleList";
-import { Navigate } from "react-router-dom";
 import LoginPage from "../pages/auths/LoginPage.jsx";
+import ProtectedRoute from "./ProtectedRoute";
+
 const routes = [
   {
     path: "/",
-    element: <TestPages />,
-  },
-  {
-    path: "/roles",
-    element: <RoleList />,
+    element: <Navigate to="/login" replace />, // default redirect to login
   },
   {
     path: "/login",
     element: <LoginPage />,
+  },
+  {
+    path: "/roles",
+    element: (
+      <ProtectedRoute
+        element={RoleList}
+        allowedRoles={["ROLE_ADMIN", "ROLE_LAB_MANAGER"]}
+      />
+    ),
+  },
+  {
+    path: "/test",
+    element: (
+      <ProtectedRoute
+        element={TestPages}
+        allowedRoles={["ROLE_USER", "ROLE_OTHER"]} // whatever other roles
+      />
+    ),
   },
 ];
 
