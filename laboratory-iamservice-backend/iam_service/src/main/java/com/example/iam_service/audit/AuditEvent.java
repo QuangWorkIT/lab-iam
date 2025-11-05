c5e4bd90ddc4f068b522382448dc539f8f518223
NguyenThiXuanGiang <nguyenxuangiangmt@gmail.com>
2025-11-05 12:48:02 +0700

    [FEAT] Add kafka config

--- laboratory-iamservice-backend/iam_service/src/main/java/com/example/iam_service/audit/AuditEvent.java
+++ laboratory-iamservice-backend/iam_service/src/main/java/com/example/iam_service/audit/AuditEvent.java
@@ -8,11 +8,12 @@ import java.time.OffsetDateTime;
 @Data
 @Builder
 public class AuditEvent {
-    private String eventType;
-    private String actor;
+    private String eventType; // change to "type"
+    private String actor; // userid
     private String target;
-    private String role;
+    private String role; // target role
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     private OffsetDateTime timestamp;
     private String details;
+    // them field source -> "iam-service"
 }
--- laboratory-iamservice-backend/iam_service/src/main/java/com/example/iam_service/audit/LogAuditPublisher.java
+++ laboratory-iamservice-backend/iam_service/src/main/java/com/example/iam_service/audit/LogAuditPublisher.java
@@ -1,23 +1,39 @@
 package com.example.iam_service.audit;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@RequiredArgsConstructor
 public class LogAuditPublisher implements AuditPublisher {
 
+    private final String TOPIC = "iam-services";
+    private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper mapper = new ObjectMapper()
             .registerModule(new JavaTimeModule());
+
+//    @Override
+//    public void publish(AuditEvent event) {
+//        try {
+//            String json = mapper.writeValueAsString(event);
+//            log.info("AUDIT_EVENT: {}", json);
+//        } catch (Exception e) {
+//            log.error("Failed to serialize AuditEvent", e);
+//        }
+//    }
 
     @Override
     public void publish(AuditEvent event) {
         try {
-            String json = mapper.writeValueAsString(event);
-            log.info("AUDIT_EVENT: {}", json);
-        } catch (Exception e) {
-            log.error("Failed to serialize AuditEvent", e);
+            String payload = mapper.writeValueAsString(event);
+            kafkaTemplate.send(TOPIC, event.getActor(), payload);
+        } catch (RuntimeException | JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
